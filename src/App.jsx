c721477e85167d55af6c2dc7a0e35bdc7bdf1323
kumarmurugan1218@gmail.com <kumarmurugan1218@gmail.com>
2025-07-10 03:01:20 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import  { useReducer ,useState } from 'react'
-  const initialstate=[{id:1,product:"reading"},{id:2,product:"writting"}];
+import  { useReducer,useEffect ,useState } from 'react'
+
+  let initialstate=[];
   const reducer=(state,action)=>{
     switch(action.type){
       case "Addcart":
@@ -13,9 +14,7 @@ import  { useReducer ,useState } from 'react'
 const App = () => {
 
   const [text,setText]=useState("")
-  const [todos,dispatch]=useReducer(reducer,initialstate)
-  console.log(text);
-  console.log(todos);
+  const [todos,dispatch]=useReducer(reducer,initialstate);
   
   
   return (
@@ -23,10 +22,12 @@ const App = () => {
       <div className='   md:w-1/2 md:px-10 shadow-2xl rounded-md p-3 bg-white'>   
         <h1 className='text-3xl text-center'>Todo App </h1>
         <div className='w-full flex gap-3 mt-2 py-1 px-2 border border-gray-500 rounded'>
-          <input type="text" onChange={(e)=>setText
+          <input type="text" value={text} onChange={(e)=>setText
             (e.target.value)} className=' w-full focus:outline-none' />
-          <button onClick={()=>{dispatch({type:"Addcart",payload:text})
-              }} onKeyDown={()=>{}} className='border px-2 py-1 rounded-md border-green-400 hover:bg-green-300 hover:text-white'>Add</button>
+          <button onClick={()=>{
+            dispatch({type:"Addcart",payload:text} );
+             setText("")
+              }}  className='border px-2 py-1 rounded-md border-green-400 hover:bg-green-300 hover:text-white'>Add</button>
           </div>
         <ul className='flex flex-col gap-5 py-3'>
           {todos.map((todo)=>(<li key={todo.id} className='flex justify-between px-5 border mx-5 py-2 rounded'>
